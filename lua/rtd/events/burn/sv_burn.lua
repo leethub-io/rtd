4baa5addb2cc7788f67461345501047ad16087a0
LeetHub <leethub.io@gmail.com>
2022-09-26 20:21:47 -0500

    Prevent damage from being taken if a burn event is triggered from RTD

--- lua/rtd/events/burn/sv_burn.lua
+++ lua/rtd/events/burn/sv_burn.lua
@@ -1,6 +1,18 @@
 -- Event function
 local function burnEventFunc(calling_ply)
-  calling_ply:Say("Gee, I'm feeling pretty hot.")
+  calling_ply:Ignite(5)
+
+  hook.Add("EntityTakeDamage", "VoidIgniteDamage", function(entity, damage)
+    if not (entity:IsPlayer() and damage:IsDamageType(DMG_BURN)) then return false end
+    
+    return true
+  end )
+  
+  timer.Simple(5, function()
+  
+    hook.Remove("EntityTakeDamage", "VoidIgniteDamage")
+
+  end )
 end
 
 RTD:RegisterEvent(burnEventFunc, "rtd_burn")
